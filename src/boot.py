f4ee89ec79993e3f4108c611f2211dd51a89c55c
gretel <code@jitter.eu>
2020-11-30 21:27:06 +0100

    first commit party :)

---
+++ src/boot.py
@@ -0,0 +1,14 @@
+#from micropython import const
+import micropython
+import gc
+
+# https://docs.micropython.org/en/latest/library/micropython.html#micropython.alloc_emergency_exception_buf
+micropython.alloc_emergency_exception_buf(100)
+
+# https://docs.micropython.org/en/latest/library/micropython.html#micropython.opt_level
+#micropython.opt_level(1)
+print('__debug__', __debug__)
+
+import machine
+# if something goes really wrong like exceptions on boot we can at least see what caused the reset
+print('reset_cause', machine.reset_cause())
